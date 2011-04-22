8a95293d15f657ccb2d5084475232dda10de2f1e
Arto Bendiken <arto@bendiken.net>
2011-04-22 07:45:04 +0200

    Created the project.

---
+++ src/machinery.h
@@ -0,0 +1,20 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _MACHINERY_H
+#define _MACHINERY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+extern const char* const machinery_version_string;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _MACHINERY_H */
