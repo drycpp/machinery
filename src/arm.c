2dda9b14dc8d08ae2db22d42e6541b47bc4bdeb7
Arto Bendiken <arto@bendiken.net>
2011-04-23 03:59:30 +0200

    Ensured that all source files include config.h and errno.h.

--- src/arm.c
+++ src/arm.c
@@ -1,4 +1,6 @@
 /* This is free and unencumbered software released into the public domain. */
 
 #include "arm.h"
+#include "config.h"
 #include <assert.h>
+#include <errno.h>
--- src/mips.c
+++ src/mips.c
@@ -1,4 +1,6 @@
 /* This is free and unencumbered software released into the public domain. */
 
 #include "mips.h"
+#include "config.h"
 #include <assert.h>
+#include <errno.h>
