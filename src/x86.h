2476cd04908ee1615fb83c93dd4f01434987a199
Arto Bendiken <arto@bendiken.net>
2011-04-26 03:42:18 +0200

    Defined constants for the x86 general-purpose registers.

--- src/x86.h
+++ src/x86.h
@@ -12,6 +12,62 @@ extern "C" {
 #include <stdlib.h>
 #include <unistd.h>   /* for ssize_t */
 
+/* x86 general-purpose registers (8-bit) */
+typedef enum {
+  X86_REG_AL = 0,  /* 0b000 */
+  X86_REG_CL,      /* 0b001 */
+  X86_REG_DL,      /* 0b010 */
+  X86_REG_BL,      /* 0b011 */
+  X86_REG_AH,      /* 0b100 */
+  X86_REG_CH,      /* 0b101 */
+  X86_REG_DH,      /* 0b110 */
+  X86_REG_BH,      /* 0b111 */
+} x86_reg8_t;
+
+/* x86 general-purpose registers (16-bit) */
+typedef enum {
+  X86_REG_AX = 0,  /* 0b000 */
+  X86_REG_CX,      /* 0b001 */
+  X86_REG_DX,      /* 0b010 */
+  X86_REG_BX,      /* 0b011 */
+  X86_REG_SP,      /* 0b100 */
+  X86_REG_BP,      /* 0b101 */
+  X86_REG_SI,      /* 0b110 */
+  X86_REG_DI,      /* 0b111 */
+} x86_reg16_t;
+
+/* x86 general-purpose registers (32-bit) */
+typedef enum {
+  X86_REG_EAX = 0, /* 0b000 */
+  X86_REG_ECX,     /* 0b001 */
+  X86_REG_EDX,     /* 0b010 */
+  X86_REG_EBX,     /* 0b011 */
+  X86_REG_ESP,     /* 0b100 */
+  X86_REG_EBP,     /* 0b101 */
+  X86_REG_ESI,     /* 0b110 */
+  X86_REG_EDI,     /* 0b111 */
+} x86_reg32_t;
+
+/* x86 general-purpose registers (64-bit) */
+typedef enum {
+  X86_REG_RAX = 0, /* 0b000 */
+  X86_REG_RCX,     /* 0b001 */
+  X86_REG_RDX,     /* 0b010 */
+  X86_REG_RBX,     /* 0b011 */
+  X86_REG_RSP,     /* 0b100 */
+  X86_REG_RBP,     /* 0b101 */
+  X86_REG_RSI,     /* 0b110 */
+  X86_REG_RDI,     /* 0b111 */
+  X86_REG_R8,
+  X86_REG_R9,
+  X86_REG_R10,
+  X86_REG_R11,
+  X86_REG_R12,
+  X86_REG_R13,
+  X86_REG_R14,
+  X86_REG_R15,
+} x86_reg32_t;
+
 /* x86 prefix byte */
 typedef uint8_t x86_prefix_t;
 
