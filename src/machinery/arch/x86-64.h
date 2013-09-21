c638ae98decb519c8e094d6f71099bdd81fa60ff
Arto Bendiken <arto@bendiken.net>
2013-09-21 21:37:20 +0200

    Added the <machinery/arch/x86-64.h> header in case users look for it.

---
+++ src/machinery/arch/x86-64.h
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef MACHINERY_ARCH_X86_64_H
+#define MACHINERY_ARCH_X86_64_H
+
+/**
+ * @file
+ */
+
+#include "x86.h"
+
+#endif /* MACHINERY_ARCH_X86_64_H */
