744208bc1c1c0f213215b3c128e12c0f3408022a
Arto Bendiken <arto@bendiken.net>
2013-09-22 08:42:49 +0200

    Implemented a trivial JIT x86 calculator example program.

---
+++ doc/examples/jit-calc.cc
@@ -0,0 +1,38 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <machinery.h>
+#include <machinery/arch/x86.h>
+
+#include <cstdio>  /* for std::fwrite() */
+#include <cstdlib> /* for std::atoi() */
+
+using namespace machinery::arch::x86;
+
+int
+main(int argc, char* argv[]) {
+  std::vector<std::uint8_t> buffer;
+  emitter code(buffer);
+
+  /* Clear the EAX register: */
+  code.emit_mov(reg32::EAX, imm32{0});
+
+  /* For each command-line argument: */
+  for (int i = 1; i < argc; i++) {
+    /* Convert the argument to an integer: */
+    const int arg = std::atoi(argv[i]);
+
+    /* Add the integer argument to EAX: */
+    code.emit_add(imm32{arg});
+  }
+
+  /* Return to the caller: */
+  code.emit_ret();
+
+#if 1
+  std::fwrite(buffer.data(), buffer.size(), 1, stdout);
+#endif
+
+  // TODO: execute the code buffer and print out the result.
+
+  return 0;
+}
