9b9c2251cfb69065f21c1b185e8b319d8e704191
Arto Bendiken <arto@bendiken.net>
2013-09-30 01:30:11 +0200

    Implemented the x86_emitter#emit_pop(reg{16,32,64}) methods.

--- src/machinery/arch/x86/emitter.h
+++ src/machinery/arch/x86/emitter.h
@@ -632,6 +632,36 @@ public:
   }
 
   /**
+   * Emits a one-byte `POP reg16` instruction.
+   *
+   * @param reg a 16-bit register operand
+   * @copydetails emit_general_purpose_instruction
+   */
+  x86_emitter& emit_pop(const x86_reg16 reg) {
+    return emit(0x58 + static_cast<std::uint8_t>(reg));
+  }
+
+  /**
+   * Emits a one-byte `POP reg32` instruction.
+   *
+   * @param reg a 32-bit register operand
+   * @copydetails emit_general_purpose_instruction
+   */
+  x86_emitter& emit_pop(const x86_reg32 reg) {
+    return emit(0x58 + static_cast<std::uint8_t>(reg));
+  }
+
+  /**
+   * Emits a one-byte `POP reg64` instruction.
+   *
+   * @param reg a 64-bit register operand
+   * @copydetails emit_general_purpose_instruction
+   */
+  x86_emitter& emit_pop(const x86_reg64 reg) {
+    return emit(0x58 + static_cast<std::uint8_t>(reg));
+  }
+
+  /**
    * Emits a one-byte `POPA` instruction.
    *
    * @copydetails emit_general_purpose_instruction
@@ -679,6 +709,7 @@ public:
   /**
    * Emits a one-byte `PUSH reg16` instruction.
    *
+   * @param reg a 16-bit register operand
    * @copydetails emit_general_purpose_instruction
    */
   x86_emitter& emit_push(const x86_reg16 reg) {
@@ -688,6 +719,7 @@ public:
   /**
    * Emits a one-byte `PUSH reg32` instruction.
    *
+   * @param reg a 32-bit register operand
    * @copydetails emit_general_purpose_instruction
    */
   x86_emitter& emit_push(const x86_reg32 reg) {
@@ -697,6 +729,7 @@ public:
   /**
    * Emits a one-byte `PUSH reg64` instruction.
    *
+   * @param reg a 64-bit register operand
    * @copydetails emit_general_purpose_instruction
    */
   x86_emitter& emit_push(const x86_reg64 reg) {
