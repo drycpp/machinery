87c06db0fc36b71beb6ded3a032ee21df9f449d7
Arto Bendiken <arto@bendiken.net>
2013-10-04 05:07:56 +0200

    Added simple FFI skeletons for Python and Ruby.

---
+++ ffi/python/machinery.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# This is free and unencumbered software released into the public domain.
+
+from ctypes import CDLL, c_bool, c_char_p, c_short
+from ctypes.util import find_library
+
+try:
+  libmachinery = CDLL('libmachinery.so.0')
+except OSError:
+  libmachinery_path = find_library('machinery')
+  if libmachinery_path:
+    libmachinery = CDLL(libmachinery_path)
+  else:
+    raise OSError('Machinery shared library not found')
+
+VERSION_STRING = c_char_p.in_dll(libmachinery, 'machinery_version_string').value
+VERSION_MAJOR  = c_short.in_dll(libmachinery, 'machinery_version_major').value
+VERSION_MINOR  = c_short.in_dll(libmachinery, 'machinery_version_minor').value
+VERSION_PATCH  = c_short.in_dll(libmachinery, 'machinery_version_patch').value
+
+def version_string(): return VERSION_STRING
+
+has_feature = libmachinery.machinery_feature_exists
+has_feature.argtypes = [c_char_p]
+has_feature.restype = c_bool
+
+has_module = libmachinery.machinery_module_exists
+has_module.argtypes = [c_char_p]
+has_module.restype = c_bool
+
+if __name__ == '__main__':
+  print "machinery.version_string(): {}".format(version_string())
+  print
+  for feature_name in ('ascii', 'debug', 'unicode'):
+    print "machinery.has_feature('{}'): {}".format(feature_name, has_feature(feature_name))
+  print
+  for module_name in ('arm', 'x86', 'ir', 'jit'):
+    print "machinery.has_module('{}'): {}".format(module_name, has_module(module_name))
+  print
---
+++ ffi/ruby/machinery.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+# This is free and unencumbered software released into the public domain.
+
+require 'ffi' # @see https://rubygems.org/gems/ffi
+
+##
+# Ruby FFI for the `libmachinery` native library.
+#
+# @see http://machinery.dryproject.org/
+module Machinery extend FFI::Library
+  ffi_lib ['machinery', 'libmachinery.so.0']
+
+  attach_variable :machinery_version_string, :string
+  attach_function :machinery_feature_exists, [:string], :bool
+  attach_function :machinery_module_exists, [:string], :bool
+
+  ##
+  # Returns the library version string.
+  #
+  # @return [String] a version string in "x.y.z" form
+  def self.version_string
+    machinery_version_string
+  end
+
+  ##
+  # Determines whether the library supports a given feature.
+  #
+  # @param  [#to_s] feature_name the name of the feature, e.g. `:ascii`
+  # @return [Boolean] `true` if the feature is supported, `false` otherwise
+  def self.has_feature?(feature_name)
+    machinery_feature_exists(feature_name.to_s)
+  end
+
+  ##
+  # Determines whether the library includes a given module.
+  #
+  # @param  [#to_s] module_name the name of the module, e.g. `:x86`
+  # @return [Boolean] `true` if the module is supported, `false` otherwise
+  def self.has_module?(module_name)
+    machinery_module_exists(module_name.to_s)
+  end
+end
+
+if __FILE__ == $0
+  puts "Machinery.version_string: %s" % Machinery.version_string
+  puts
+  %w(ascii debug unicode).each do |feature_name|
+    puts "Machinery.has_feature?(:#{feature_name}): %s" % Machinery.has_feature?(feature_name)
+  end
+  puts
+  %w(arm x86 ir jit).each do |module_name|
+    puts "Machinery.has_module?(:#{module_name}) %s" % Machinery.has_module?(module_name)
+  end
+  puts
+end
