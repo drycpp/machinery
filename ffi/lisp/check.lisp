9cad409c3fae71064efa80114edfe6eb97ea2e7e
Arto Bendiken <arto@bendiken.net>
2013-10-05 07:12:58 +0200

    Added `make check` targets for all FFI implementations.

---
+++ ffi/lisp/check.lisp
@@ -0,0 +1,27 @@
+(require 'machinery)
+
+(machinery:load-library)
+
+(format t "~S~&" (list 'machinery:+version-string+ machinery:+version-string+))
+
+(format t "~S~&" (list 'machinery:+version-major+ machinery:+version-major+))
+
+(format t "~S~&" (list 'machinery:+version-minor+ machinery:+version-minor+))
+
+(format t "~S~&" (list 'machinery:+version-patch+ machinery:+version-patch+))
+
+(format t "~S~&"
+  (list 'machinery:featurep
+        (mapcar (lambda (feature-name)
+                  (list feature-name (machinery:featurep feature-name)))
+                '("ascii" "debug" "unicode"))))
+
+(format t "~S~&"
+  (list 'machinery:modulep
+        (mapcar (lambda (module-name)
+                  (list module-name (machinery:modulep module-name)))
+                '("arm" "x86" "ir" "jit"))))
+
+(format t "~&")
+
+(machinery:unload-library)
