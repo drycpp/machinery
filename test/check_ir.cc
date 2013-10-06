ba20a6dae03bc7267f5a7d68e117f5ee91cee87c
Arto Bendiken <arto@bendiken.net>
2013-10-06 05:10:54 +0200

    Ensured that unit tests include the <config.h> header file.

--- test/check_ir.cc
+++ test/check_ir.cc
@@ -1,5 +1,9 @@
 /* This is free and unencumbered software released into the public domain. */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #define BOOST_TEST_MODULE ir
 #include <boost/test/unit_test.hpp>
 
