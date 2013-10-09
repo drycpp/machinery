024abfab46653a5159eb244b73244ae10b752331
Arto Bendiken <arto@bendiken.net>
2013-10-09 07:15:16 +0200

    Sketched out some initial JIT compiler instructions.

--- src/machinery/jit/compiler.h
+++ src/machinery/jit/compiler.h
@@ -81,9 +81,150 @@ public:
   compiler& operator=(compiler&& other) noexcept = delete;
 
   /**
-   * Emits a `NOP` instruction.
+   * @name Control Instructions
+   */
+
+  /**@{*/
+
+  /**
+   * Emits an `ENTER` (enter stack frame) instruction.
+   */
+  virtual compiler& enter() = 0;
+
+  /**
+   * Emits a `JMP` (jump) instruction.
+   */
+  virtual compiler& jmp(...) = 0;
+
+  /**
+   * Emits a `LEAVE` (leave stack frame) instruction.
+   */
+  virtual compiler& leave() = 0;
+
+  /**
+   * Emits a `RET` (return) instruction.
+   */
+  virtual compiler& ret() = 0;
+
+  /**@}*/
+
+  /**
+   * @name Basic Instructions
+   */
+
+  /**@{*/
+
+  /**
+   * Emits an `ABS` (absolute value) instruction.
+   */
+  virtual compiler& abs(...) = 0;
+
+  /**
+   * Emits an `ADD` (add) instruction.
+   */
+  virtual compiler& add(...) = 0;
+
+  /**
+   * Emits an `AND` (bitwise AND) instruction.
+   */
+  virtual compiler& and_(...) = 0;
+
+  /**
+   * Emits a `CLZ` (count leading zeroes) instruction.
+   */
+  virtual compiler& clz(...) = 0;
+
+  /**
+   * Emits a `CMP` (compare) instruction.
+   */
+  virtual compiler& cmp(...) = 0;
+
+  /**
+   * Emits a `DEC` (decrement) instruction.
+   */
+  virtual compiler& dec(...) = 0;
+
+  /**
+   * Emits a `DIV` (divide) instruction.
+   */
+  virtual compiler& div(...) = 0;
+
+  /**
+   * Emits an `INC` (increment) instruction.
+   */
+  virtual compiler& inc(...) = 0;
+
+  /**
+   * Emits a `MOV` (move) instruction.
+   */
+  virtual compiler& mov(...) = 0;
+
+  /**
+   * Emits a `MUL` (multiply) instruction.
+   */
+  virtual compiler& mul(...) = 0;
+
+  /**
+   * Emits a `NAND` (bitwise NAND) instruction.
+   */
+  virtual compiler& nand(...) = 0;
+
+  /**
+   * Emits a `NEG` (negate) instruction.
+   */
+  virtual compiler& neg(...) = 0;
+
+  /**
+   * Emits a `NOP` (no operation) instruction.
    */
   virtual compiler& nop() = 0;
+
+  /**
+   * Emits a `NOR` (bitwise NOR) instruction.
+   */
+  virtual compiler& nor(...) = 0;
+
+  /**
+   * Emits a `NOT` (bitwise NOT) instruction.
+   */
+  virtual compiler& not_(...) = 0;
+
+  /**
+   * Emits an `OR` (bitwise OR) instruction.
+   */
+  virtual compiler& or_(...) = 0;
+
+  /**
+   * Emits a `POW` (power) instruction.
+   */
+  virtual compiler& pow(...) = 0;
+
+  /**
+   * Emits a `REM` (remainder) instruction.
+   */
+  virtual compiler& rem(...) = 0;
+
+  /**
+   * Emits an `SHL` (shift left) instruction.
+   */
+  virtual compiler& shl(...) = 0;
+
+  /**
+   * Emits an `SHR` (shift right) instruction.
+   */
+  virtual compiler& shr(...) = 0;
+
+  /**
+   * Emits a `SUB` (subtract) instruction.
+   */
+  virtual compiler& sub(...) = 0;
+
+  /**
+   * Emits a `XOR` (exclusive-or) instruction.
+   */
+  virtual compiler& xor_(...) = 0;
+
+  /**@}*/
 };
 
 #endif /* MACHINERY_JIT_COMPILER_H */
